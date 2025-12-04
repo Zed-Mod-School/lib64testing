2b126a3fef3237cef8d663502ad2db16abf58c02
Zedb0T <89345505+Zedb0T@users.noreply.github.com>
2025-12-03 22:26:37 -0500

    Disable test music and skip collide for periscope in jungle (crash)

--- game/kernel/jak1/Mario1.cpp
+++ game/kernel/jak1/Mario1.cpp
@@ -65,7 +65,7 @@ int load_and_init_mario() {
   }
   audio_init();
   //This is "test" music to make sure audio is working
-  sm64_play_music(0, 0x05 | 0x80, 0);
+  //sm64_play_music(0, 0x05 | 0x80, 0);
   // for (int i = 0; i < 10; ++i) {
   //   printf("marioId = %d\n", marioId);
   // }
