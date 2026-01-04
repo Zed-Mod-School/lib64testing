504bae19194852ecc7661551c19f3a14d8d443eb
Zedb0T <89345505+Zedb0T@users.noreply.github.com>
2026-01-04 00:25:35 -0500

    Remove alot of triangle and cylinder collide stuff

--- game/kernel/jak1/Mario1.cpp
+++ game/kernel/jak1/Mario1.cpp
@@ -612,22 +612,7 @@ static inline float to_f32(int32_t val) {
   return static_cast<float>(val);
 }
 
-// Check if point is inside triangle in XZ plane
-bool point_in_triangle_2d(float px, float pz, const int32_t v[3][3]) {
-  float x0 = to_f32(v[0][0]), z0 = to_f32(v[0][2]);
-  float x1 = to_f32(v[1][0]), z1 = to_f32(v[1][2]);
-  float x2 = to_f32(v[2][0]), z2 = to_f32(v[2][2]);
 
-  float dX = px - x2;
-  float dZ = pz - z2;
-  float dX21 = x2 - x1;
-  float dZ12 = z1 - z2;
-  float D = (z0 - z2) * dX21 + (x2 - x0) * dZ12;
-  float s = ((z0 - z2) * dX + (x2 - x0) * dZ) / D;
-  float t = ((z2 - z1) * dX + (x1 - x2) * dZ) / D;
-
-  return s >= 0 && t >= 0 && (s + t) <= 1;
-}
 
 // Sample points inside triangle to check for cylinder overlap
 bool triangle_samples_in_cylinder(float center_x, float center_z, const int32_t v[3][3]) {
@@ -697,56 +682,6 @@ void load_combined_static_surfaces(const SM64Surface* surfaces1,
   }
 }
 
-float triangle_area_2d(const int32_t v[3][3]) {
-  float x0 = to_f32(v[0][0]), z0 = to_f32(v[0][2]);
-  float x1 = to_f32(v[1][0]), z1 = to_f32(v[1][2]);
-  float x2 = to_f32(v[2][0]), z2 = to_f32(v[2][2]);
-  return 0.5f * fabsf((x1 - x0) * (z2 - z0) - (x2 - x0) * (z1 - z0));
-}
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-bool triangle_intersects_cylinder_recursive(float cx,
-                                            float cz,
-                                            const int32_t v[3][3],
-                                            int depth = 0) {
-  if (depth > 3) {
-    return triangle_samples_in_cylinder(cx, cz, v);
-  }
-
-  float area = triangle_area_2d(v);
-  const float cylinder_area = M_PI * CYLINDER_RADIUS_SQ;
-
-  if (area < cylinder_area * 4) {
-    return triangle_samples_in_cylinder(cx, cz, v);
-  }
-
-  // Compute midpoints
-  int32_t m01[3], m12[3], m20[3];
-  for (int i = 0; i < 3; ++i) {
-    m01[i] = (v[0][i] + v[1][i]) / 2;
-    m12[i] = (v[1][i] + v[2][i]) / 2;
-    m20[i] = (v[2][i] + v[0][i]) / 2;
-  }
-
-  // Subdivide into 4 triangles and test recursively
-  int32_t sub1[3][3] = {
-      {v[0][0], v[0][1], v[0][2]}, {m01[0], m01[1], m01[2]}, {m20[0], m20[1], m20[2]}};
-  int32_t sub2[3][3] = {
-      {m01[0], m01[1], m01[2]}, {v[1][0], v[1][1], v[1][2]}, {m12[0], m12[1], m12[2]}};
-  int32_t sub3[3][3] = {
-      {m20[0], m20[1], m20[2]}, {m12[0], m12[1], m12[2]}, {v[2][0], v[2][1], v[2][2]}};
-  int32_t sub4[3][3] = {
-      {m01[0], m01[1], m01[2]}, {m12[0], m12[1], m12[2]}, {m20[0], m20[1], m20[2]}};
-
-  return triangle_intersects_cylinder_recursive(cx, cz, sub1, depth + 1) ||
-         triangle_intersects_cylinder_recursive(cx, cz, sub2, depth + 1) ||
-         triangle_intersects_cylinder_recursive(cx, cz, sub3, depth + 1) ||
-         triangle_intersects_cylinder_recursive(cx, cz, sub4, depth + 1);
-}
-
 int load_surfaces_near(float x, float y, float z) {
   if (!g_combined_surfaces || g_combined_surfaces_count == 0)
     return 0;
@@ -769,16 +704,6 @@ int load_surfaces_near(float x, float y, float z) {
         include = true;
         break;
       }
-    }
-
-    // 2. Cylinder center inside triangle
-    if (!include && point_in_triangle_2d(x, z, s->vertices)) {
-      include = true;
-    }
-
-    // 3. Triangle samples intersect cylinder
-    if (!include && triangle_intersects_cylinder_recursive(x, z, s->vertices)) {
-      include = true;
     }
 
     if (include) {
